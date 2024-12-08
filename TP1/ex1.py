234f84c07f28dc307e1d5bb5456ff2a6710da0dc
soumatomzen <soumayatomzen784@gmail.com>
2024-12-08 16:22:17 +0100

    first commit

---
+++ TP1/ex1.py
@@ -0,0 +1,7 @@
+
+
+def somme_liste(liste):
+    return sum(liste)
+liste=[99,88,77,22]
+print(somme_liste(liste))
+
---
+++ TP1/ex2.py
@@ -0,0 +1,7 @@
+
+def max_tuple(t):
+    if not t:
+       return none
+    return max(t)
+t=(88,77,44,66)
+print(max_tuple(t))
